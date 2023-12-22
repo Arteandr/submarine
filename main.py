548709d414dd50b91fa5e29c1a89a69292596234
Arteandr <artedamatik@gmail.com>
2023-12-22 17:03:34 +0300

    init

---
+++ main.py
@@ -0,0 +1,64 @@
+import sys
+import math
+import PyQt5.QtWidgets as qt_widgets
+from PyQt5 import uic
+from PyQt5.QtCore import QLocale
+from PyQt5.QtWidgets import QApplication, QOpenGLWidget, QMessageBox
+from PyQt5.QtGui import QDoubleValidator
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from vizualize import *
+import pyvista as pv
+import numpy as np
+
+        
+class MainUI(qt_widgets.QMainWindow):
+    def __init__(self):
+        super(MainUI, self).__init__()
+        uic.loadUi('main_menu.ui', self)
+        self.vizualize_btn = self.findChild(qt_widgets.QPushButton, 'vizualize_button')
+        self.vizualize_btn.clicked.connect(self.onVizualizeButtonClick)
+        self.a_input = self.findChild(qt_widgets.QLineEdit, 'a_input')
+        self.a_input.setValidator(QDoubleValidator(0.0, 100.0, 2, self).setLocale(QLocale("en_US")))
+        self.b_input = self.findChild(qt_widgets.QLineEdit, 'b_input')
+        self.b_input.setValidator(QDoubleValidator(0.0, 100.0, 2, self).setLocale(QLocale("en_US")))
+        self.c_input = self.findChild(qt_widgets.QLineEdit, 'c_input')
+        self.c_input.setValidator(QDoubleValidator(0.0, 100.0, 2, self).setLocale(QLocale("en_US")))
+        self.angle_input = self.findChild(qt_widgets.QLineEdit, 'angle_input')
+        self.angle_input.setValidator(QDoubleValidator(0.0, 100.0, 2, self).setLocale(QLocale("en_US")))
+        self.color_group = self.findChild(qt_widgets.QGroupBox, 'color_group')
+        
+        
+        self.show()
+    
+    def get_selected_color(self, number) -> None:
+        r = self.findChild(qt_widgets.QRadioButton, number + "_color_r") 
+        g = self.findChild(qt_widgets.QRadioButton, number + "_color_g") 
+        b = self.findChild(qt_widgets.QRadioButton, number + "_color_b") 
+        
+        if r.isChecked():
+            return "red"
+        elif g.isChecked():
+            return "green"
+        elif b.isChecked():
+            return "blue"
+    
+    def onVizualizeButtonClick(self):
+        """
+        if len(self.angle_input.text()) == 0 or len(self.a_input.text()) == 0 or len(self.b_input.text()) == 0 or len(self.c_input.text()) == 0:
+            QMessageBox.about(self, "Ошибка", "Введите все значения!")
+            return
+        t = (float(self.a_input.text()), float(self.b_input.text()), float(self.c_input.text())) 
+        angle = float(self.angle_input.text())
+        widget = OpenGLWidget('sup.stl', t, angle, (self.get_selected_color("f"), self.get_selected_color("s")))
+        """
+        
+        global viz_window 
+        viz_window = VizualizeUI()
+        viz_window.show()
+
+if __name__ == '__main__':
+    app= QApplication(sys.argv)
+    main_window = MainUI()
+    sys.exit(app.exec_())
---
+++ vizualize.py
@@ -0,0 +1,93 @@
+from PyQt5.QtCore import Qt
+from PyQt5 import uic
+import PyQt5.QtWidgets as QtWidgets
+from PyQt5.QtWidgets import QWidget
+import numpy as np
+import pyvista as pv
+import pyvistaqt as pvqt
+import math
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+class VizualizeUI(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(VizualizeUI, self).__init__()
+        self.initUI()
+        
+        self.front_view = self.findChild(QtWidgets.QGroupBox, "front_view")
+        self.back_view = self.findChild(QtWidgets.QGroupBox, "back_view")
+        self.global_view = self.findChild(QtWidgets.QGroupBox, "global_view")
+        self.global_view_l= self.findChild(QtWidgets.QVBoxLayout, "v_global_view")
+        
+        global_interactor = self.global_view = SubmarineGLWidget('sup.stl',(200,0,100), 45, ("red","red"), self.global_view).get_widget()
+        self.global_view_l.addWidget(global_interactor, stretch=1)
+        
+    def initUI(self):
+        uic.loadUi('./vizualize.ui',self)
+        
+ 
+class SubmarineGLWidget(QtWidgets.QOpenGLWidget):
+    start_pos = np.array([124,0,-4])
+    
+    def __init__(self, stl_file, pos, angle, colors, parent):
+        fColor, sColor = colors
+        self.pos = pos 
+        self.angle = angle
+        super().__init__()
+        self.mesh = pv.read(stl_file)
+        l1,l2 = self.draw_lines()
+        #pl = pv.Plotter()
+        self.pl = pvqt.QtInteractor(parent=parent)
+        self.pl.add_mesh(self.mesh, color='white')
+        self.pl.add_mesh(l1, color=fColor, line_width=5)
+        self.pl.add_mesh(l2, color=sColor, line_width=5)
+        self.pl.add_axes()
+        #pl.show()
+        
+    def get_widget(self):
+        return self.pl
+    
+    def draw_lines(self):
+        endpoint = self.calculate_endpoint()
+        topLine = pv.Line(tuple(self.start_pos.tolist()), endpoint)
+        # end_point_second = self.start_pos + 100 * np.array([np.cos(-self.angle), 0, np.sin(-self.angle)])
+        bottomLine = pv.Line(tuple(self.start_pos.tolist()), self.calculate_coordinates(endpoint))
+        
+        return (topLine, bottomLine)
+    
+    def calculate_coordinates(self, pos):
+        x1,y1,z1 = pos
+        angle_rad = math.radians(self.angle)
+        z_offset = math.sin(angle_rad)
+        z2 = z1 + z_offset
+        print(z1)
+        print(z2)
+        
+        return x1, y1, z2
+    
+    def calculate_endpoint(self):
+        x,y,z = self.pos
+        pos_np = np.array([x,y,z])
+        end_point = pos_np + self.start_pos
+        return tuple(end_point.tolist())
+        
+
+    def initializeGL(self):
+        glClearColor(0.0, 0.0, 0.0, 1.0)
+        glClearDepth(1.0)
+        glEnable(GL_DEPTH_TEST)
+        glDepthFunc(GL_LEQUAL)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45, 1, 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
+        
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        glTranslate(0.0, 0.0, -5.0)
+        self.mesh.plot(opacity=1)
+
+    def resizeGL(self, width, height):
+        glViewport(0, 0, width, height)
